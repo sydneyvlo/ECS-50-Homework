c08c8ef6eb0f6af46605f77cbf022b5333dace77
Sydney <sydneyvlo@gmail.com>
2014-07-28 00:41:23 -0700

    Saving registers, setting up the base pointer, and creating one local variable.

--- hw4/sort.asm
+++ hw4/sort.asm
@@ -1,9 +1,17 @@
-; Sydney Lo
-; Integer sorting function using insertion sort.
-; void sort (int array, int arraySize);
-
-section	.data
 section	.text
-	global	_start
-	_start:
+; Sydney Lo
+; Integer sorting function using insertion sort. Register neutral.
+; void sort (int array, int arraySize);
+; Let's use di as holding the current position and si to hold the array.
+
+	global	my_sort
+my_sort:
+	push ebp	; Save the old stack pointer
+	mov	ebp, esp	; Move the stack pointer onto the base pointer(frame pointer in CUSP)
+	push esi	; Save the old esi register
+	push edi	; Save the old edi register
+	sub esp, 4	; Making room for one local variable
+		
+	
+	
 	
